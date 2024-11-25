deb6297b57e0dd051f7c1e255f175fceaab21eea
Adarshit Soni <adarshitsoni@gmail.com>
2024-11-25 09:13:01 +0530

    day 03

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 // React Element=> Object =>  HTML Element(render)
 
 const heading = React.createElement('h1',{id:'heading'},"Hello React From Adarshit")
-//jsx
+
+//JsX - it is kind of convention that helps to write a html like markup in javascript (i.e it is not html in js)
+
+
+const jsxheading= <h1> Heading using JSX</h1>
+//it is not a valid js but also a valid js
+
+console.log(jsxheading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);  
+root.render(jsxheading);  
