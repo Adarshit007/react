c5215d6b20c6e9d239dcb7056aaa3c475b601568
Adarshit Soni <adarshitsoni@gmail.com>
2024-11-28 09:18:04 +0530

    day 04

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 
 
@@ -20,18 +20,32 @@ import ReactDOM from 'react-dom';
 
 
 const Header= () =>{
-    return
+    return(
+        <div className="header">
+            <div className='logo-container'>
+                <img className='logo' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt='logo'/>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>  
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 
 const AppLayout= ()=>{
     return(
-        <div class='app'>
-            //header
+        <div className='app'>
+            <Header/>
 
         </div>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading); 
-root.render(title);
+root.render(<AppLayout/>);
